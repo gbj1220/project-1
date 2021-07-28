855e625e0898d062680d6031cf79a3fa3f8ca3ed
Patrick <patnavarre@gmail.com>
2021-07-28 15:11:57 -0400

    added table to ShoppingCart.js and add the ShoppingCart to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import HomePage from './components/HomePage/HomePage';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
+			<ShoppingCart/>
 			<HomePage />
+			
 		</div>
 	);
 }
--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,5 +1,57 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(product, price, quantity) {
+  return { product, price, quantity };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 export default function ShoppingCart() {
-	return <div>ShoppingCart</div>;
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity&nbsp;(g)</TableCell>
+			<TableCell align="right">Total: </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.product}>
+              <TableCell component="th" scope="row">
+                {row.product}
+              </TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
