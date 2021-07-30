9737dc6cfcb850712303a4acffce5ca6ee8123f5
Gregory.Johnson <3531op@gmail.com>
2021-07-30 11:29:26 -0400

    Created an initial state in context.js

--- src/context/context.js
+++ src/context/context.js
@@ -1,19 +1,29 @@
 import { createContext, useReducer } from 'react';
 
+const initialState = {
+	isAuth: false,
+	user: null,
+};
+
 const signInReducer = (state, action) => {
 	switch (action.type) {
 		case 'SIGN_IN':
-			console.log(action.payload.username);
+			console.log('SIGN_IN ran');
+			console.log(action.payload);
 			break;
+
 		default:
 			console.log('signInReducerRan');
 			console.log(action.payload);
 	}
 };
+
 export const mainContext = createContext({});
+
 // Can also pass in props.children and get rid of the object for the param i.e. const MainContextProvider = (props.children)
 const MainContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(signInReducer, []);
+	const [state, dispatch] = useReducer(signInReducer, initialState);
+
 	return (
 		<mainContext.Provider value={{ state, dispatch }}>
 			{children}
